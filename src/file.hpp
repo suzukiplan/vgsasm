97f9ed8ff187752a6e8a6d472cd13fdcefc05714
suzukiplan <suzukiplan.ys@gmail.com>
2024-10-12 17:49:49 +0900

    implement #include

--- src/file.hpp
+++ src/file.hpp
@@ -6,6 +6,8 @@ enum class TokenType {
     Other    // その他
 };
 
+static std::vector<std::string> includeFiles;
+
 class LineData
 {
   public:
@@ -193,7 +195,37 @@ std::vector<LineData*> readFile(const char* filePath)
     while (first < str.size()) {
         std::string subStr(str, first, last - first);
         auto line = new LineData(filePath, ++lineNumber, subStr);
-        result.push_back(line);
+
+        // #Iinclude は他のプリプロセッサよりも優先して先に展開
+        if (2 == line->token.size() && 0 == strcmp("#INCLUDE", line->token[0].second.c_str()) && TokenType::String == line->token[1].first) {
+            auto path = line->token[1].second.c_str();
+            bool alreadyIncluded = false;
+            for (auto includeFile : includeFiles) {
+                if (0 == strcmp(includeFile.c_str(), path)) {
+                    alreadyIncluded = true;
+                }
+            }
+            if (!alreadyIncluded) {
+                includeFiles.push_back(path);
+                char basePath[4096];
+                if ('/' != path[0]) {
+                    strcpy(basePath, line->path.c_str());
+                    char* cp = strrchr(basePath, '/');
+                    if (cp) {
+                        cp++;
+                        *cp = 0;
+                    }
+                    strcat(basePath, path);
+                    path = basePath;
+                }
+                for (auto includeLine : readFile(path)) {
+                    result.push_back(includeLine);
+                }
+            }
+        } else {
+            result.push_back(line);
+        }
+
         first = last + 1;
         last = str.find_first_of('\n', first);
         if (last == std::string::npos) {
