e95e8da41317a2b340b0657c7620391ddbe2b4b8
suzukiplan <suzukiplan.ys@gmail.com>
2024-10-14 12:58:25 +0900

    Bracket operation priority bug fixed

--- src/formulas.hpp
+++ src/formulas.hpp
@@ -2,7 +2,7 @@
 #include "common.hpp"
 #include "file.hpp"
 
-void evaluate_formulas(LineData* line)
+std::string evaluate_formulas(std::vector<std::pair<TokenType, std::string>>* token)
 {
     std::vector<std::pair<TokenType, std::string>>::iterator prev;
     std::vector<std::pair<TokenType, std::string>>::iterator next;
@@ -12,30 +12,33 @@ void evaluate_formulas(LineData* line)
     calc = true;
     while (calc) {
         calc = false;
-        prev = line->token.end();
-        for (auto it = line->token.begin(); it != line->token.end(); it++) {
+        prev = token->end();
+        for (auto it = token->begin(); it != token->end() && it->first != TokenType::BracketEnd; it++) {
+            if (it->first == TokenType::BracketBegin) {
+                std::vector<std::pair<TokenType, std::string>> work;
+                for (auto itW = it + 1; itW->first != TokenType::BracketEnd; token->erase(itW)) {
+                    work.push_back(std::make_pair(itW->first, itW->second));
+                }
+                evaluate_formulas(&work);
+                token->erase(it + 1); // remove )
+                token->erase(it);     // remove (
+                for (int i = 0; i < work.size(); i++) {
+                    token->insert(it + i, work[i]);
+                }
+                it = token->begin();
+            }
             if (it->first == TokenType::Mul || it->first == TokenType::Div) {
                 // 直前のトークンが　Numeric 型かチェック
-                if (line->token.end() == prev || prev->first != TokenType::Numeric) {
-                    // 直前のトークンが BracketEnd の場合は無視
-                    if (line->token.end() != prev && prev->first == TokenType::BracketEnd) {
-                        continue;
-                    }
-                    line->error = true;
-                    line->errmsg = "Illegal expression with no specification of the number of operations to be performed.";
-                    puts(prev->second.c_str());
-                    return;
+                if (token->end() == prev || prev->first != TokenType::Numeric) {
+                    return "Illegal expression with no specification of the number of operations to be performed.";
                 }
                 // 次のトークンが　Numeric 型かチェック
                 next = it + 1;
-                if (next == line->token.end() || next->first != TokenType::Numeric) {
-                    // 次のトークンが BracketBegin の場合は無視
-                    if (line->token.end() != next && next->first == TokenType::BracketBegin) {
+                if (next == token->end() || next->first != TokenType::Numeric) {
+                    if (next != token->end() && TokenType::BracketBegin == next->first) {
                         continue;
                     }
-                    line->error = true;
-                    line->errmsg = "Illegal expression with no specification of the number of operations to be performed.";
-                    return;
+                    return "Illegal expression with no specification of the number of operations to be performed.";
                 }
                 // 計算を実行
                 // printf("calc: %s %s %s\n", prev->second.c_str(), it->second.c_str(), next->second.c_str());
@@ -45,15 +48,13 @@ void evaluate_formulas(LineData* line)
                 } else {
                     int value = atoi(next->second.c_str());
                     if (0 == value) {
-                        line->error = true;
-                        line->errmsg = "Division by zero.";
-                        return;
+                        return "Division by zero.";
                     }
                     result /= value;
                 }
                 prev->second = std::to_string(result); // prev を演算結果に書き換え
-                line->token.erase(next);               // 掛けた or 割った 数は削除
-                line->token.erase(it);                 // * or / は削除
+                token->erase(next);                    // 掛けた or 割った 数は削除
+                token->erase(it);                      // * or / は削除
                 calc = true;                           // 演算を実行したので再度評価を実施
                 break;
             }
@@ -65,34 +66,25 @@ void evaluate_formulas(LineData* line)
     calc = true;
     while (calc) {
         calc = false;
-        prev = line->token.end();
-        for (auto it = line->token.begin(); it != line->token.end(); it++) {
+        prev = token->end();
+        for (auto it = token->begin(); it != token->end() && it->first != TokenType::BracketEnd; it++) {
             if (it->first == TokenType::Plus || it->first == TokenType::Minus) {
                 // 直前のトークンが　Numeric 型かチェック
-                if (line->token.end() == prev || prev->first != TokenType::Numeric) {
+                if (token->end() == prev || prev->first != TokenType::Numeric) {
                     // 直前のトークンが Operand の場合は無視（IX+dなどに対応するため）
-                    if (line->token.end() != prev && prev->first == TokenType::Operand) {
+                    if (token->end() != prev && prev->first == TokenType::Operand) {
                         continue;
                     }
-                    // 直前のトークンが BracketEnd の場合は無視
-                    if (line->token.end() != prev && prev->first == TokenType::BracketEnd) {
-                        continue;
-                    }
-                    line->error = true;
-                    line->errmsg = "Illegal expression with no specification of the number of operations to be performed.";
-                    puts(prev->second.c_str());
-                    return;
+                    return "Illegal expression with no specification of the number of operations to be performed.";
                 }
                 // 次のトークンが　Numeric 型かチェック
                 next = it + 1;
-                if (next == line->token.end() || next->first != TokenType::Numeric) {
+                if (next == token->end() || next->first != TokenType::Numeric) {
                     // 次のトークンが BracketBegin の場合は無視
-                    if (line->token.end() != next && next->first == TokenType::BracketBegin) {
+                    if (token->end() != next && next->first == TokenType::BracketBegin) {
                         continue;
                     }
-                    line->error = true;
-                    line->errmsg = "Illegal expression with no specification of the number of operations to be performed.";
-                    return;
+                    return "Illegal expression with no specification of the number of operations to be performed.";
                 }
                 // 計算を実行
                 // printf("calc: %s %s %s\n", prev->second.c_str(), it->second.c_str(), next->second.c_str());
@@ -103,12 +95,22 @@ void evaluate_formulas(LineData* line)
                     result -= atoi(next->second.c_str());
                 }
                 prev->second = std::to_string(result); // prev を演算結果に書き換え
-                line->token.erase(next);               // 足した or引いた 数は削除
-                line->token.erase(it);                 // * or / は削除
+                token->erase(next);                    // 足した or引いた 数は削除
+                token->erase(it);                      // * or / は削除
                 calc = true;                           // 演算を実行したので再度評価を実施
                 break;
             }
             prev = it;
         }
+    }
+    return "";
+}
+
+void evaluate_formulas(LineData* line)
+{
+    auto errmsg = evaluate_formulas(&line->token);
+    if (!errmsg.empty()) {
+        line->error = true;
+        line->errmsg = errmsg;
     }
 }
