c6357f1f8a8d3530be3988b501592da07454f94a
suzukiplan <suzukiplan.ys@gmail.com>
2024-10-29 22:25:53 +0900

    output error source .asm

--- tests/check.cpp
+++ tests/check.cpp
@@ -8,19 +8,22 @@ int main(int argc, char* argv[])
         puts("Invalid argument number");
         exit(-1);
     }
+    char* src = argv[1] + 7;
     char* cmp = strstr(argv[1], ") ");
     if (cmp) {
         cmp = cmp + 2;
     } else {
         cmp = argv[1];
     }
+    char* cp = strstr(src, " (");
+    if (cp) { *cp = 0; }
     char* lf = strchr(cmp, '\n');
     if (lf) { *lf = 0; }
     if (0 == strcmp(cmp, argv[2])) {
         printf("E: %s ... OK\n", cmp);
         return 0;
     } else {
-        printf("E: %s ... NG!\n", cmp);
+        printf("E: %s ... NG! <%s>\n", cmp, src);
         return -1;
     }
 }
