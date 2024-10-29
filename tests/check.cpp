133a935766c7c8b3228c6a7afc6f0d8728963bc3
suzukiplan <suzukiplan.ys@gmail.com>
2024-10-29 10:08:23 +0900

    make unit test system for struct

---
+++ tests/check.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        puts("Invalid argument number");
+        exit(-1);
+    }
+    if (0 == strcmp(argv[1], argv[2])) {
+        printf("E: %s ... OK\n", argv[1]);
+        return 0;
+    } else {
+        printf("E: %s ... NG! <%s>\n", argv[1], argv[2]);
+        return -1;
+    }
+}
